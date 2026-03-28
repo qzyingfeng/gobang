a6226285d1975a6890054d1e8fea158e01000bc1
test <test@example.com>
2026-03-28 23:11:24 +0800

    发布最新版本

---
+++ src/settings.70556.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Login.fire",orientation:"",jsList:[],bundleVers:{internal:"620a7",resources:"47ace",main:"64519"}};
