0cf8e746b78ee7179f88dba5e24012edf3e0c090
test <test@example.com>
2026-05-05 13:33:40 +0800

    update web build

---
+++ src/settings.98372.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/mian.fire",orientation:"",jsList:[],bundleVers:{internal:"620a7",resources:"122a8",main:"9d65a"}};
